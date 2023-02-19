844dc69e572790b3a7516f884b12429ffef12584
Luka Vashakmadze <50746021+Vashakmadze@users.noreply.github.com>
2023-02-19 19:33:03 +0100

    dalleroutes done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/post', dalleRouts);
+app.use('/api/v1/dalle', dalleRouts);
 
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E');
