341edfeffa2068de639b5b90f5fced335810ae9d
luka.vashakmadze <geogamerstime@gmail.com>
2024-02-16 13:22:17 +0100

    added cors origin

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: '*'
+  }
+));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/post', postRoutes);
